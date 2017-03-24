49b7486db2a7800e448344de4b9ee2043f1ed2b8
Grinil Khanna <gmkhanna@gmail.com>
2017-03-24 13:45:43 -0700

    Add find and link to detail page - need to add update to detail page.

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,17 +4,10 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
   },
-
   actions : {
-
-  saveQuestion(params) {
-    var newQuestion = this.store.createRecord('question', params);
-    newQuestion.save();
-    this.transitionTo('index');
-  },
-
-    destroyQuestion(question) {
-      question.destroyRecord();
+    saveQuestion(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
       this.transitionTo('index');
     }
   }
