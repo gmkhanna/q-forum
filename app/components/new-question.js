7301946bd7792647752941247bb124e0968ca599
Grinil Khanna <gmkhanna@gmail.com>
2017-03-24 11:57:47 -0700

    Add working save function on index

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  actions: {
+    saveQuestion() {
+      var params = {
+        author: this.get('author'),
+        content: this.get('content'),
+        notes: this.get('notes'),
+      };
+      this.sendAction('saveQuestion', params);
+    }
+  }
 });
